99d0541e65d79546aa9120dec4c2b65690f015ab
Денис Бекк <denisbeck555@gmail.com>
2021-01-16 20:10:58 +0300

    Создаём viewModel

---
+++ app/src/main/java/com/example/kursovaya/screens/MoviesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.kursovaya.screens
+
+import androidx.lifecycle.ViewModel
+
+// Создаём viewModel Для архитектуроного подхода MVVM (Model View ViewModel)
+class MoviesViewModel() : ViewModel() {
+}
