a3edecfa9eaa83f97d7c62b83dc34b273c88103c
Денис Бекк <denisbeck555@gmail.com>
2021-01-16 20:20:33 +0300

    добавляем сетевые клиенты

---
+++ app/src/main/java/com/example/kursovaya/networking/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.example.kursovaya.networking
+
+import com.example.kursovaya.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+// Создаём retrofit client для обработки сетевых запросов
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl(BuildConfig.API_URL).client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+// создаём okHttp клиент для сетевых запросов
+fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+    return OkHttpClient().newBuilder().addInterceptor(authInterceptor).build()
+}
